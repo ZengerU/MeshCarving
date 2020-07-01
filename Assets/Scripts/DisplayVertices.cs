1848f3f42ea8c541d81b69b692a27695595e714f
Umut zenger <zengerumut@gmail.com>
2020-07-01 22:57:47 +0300

    initial commit

---
+++ Assets/Scripts/DisplayVertices.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayVertices : MonoBehaviour
+{
+    private Vector3[] vertices;
+    [SerializeField]
+    private GameObject verticePrefab;
+    [SerializeField]
+    private Transform verticeParent;
+    [SerializeField]
+    private Button generateButton;
+
+    public void generateVertices()
+    {
+       foreach(Transform child in verticeParent)
+        {
+            Destroy(child.gameObject);
+        }
+        vertices = GetComponent<MeshFilter>().mesh.vertices;
+        Color color = new Color(1, 0, 1, 1.0f);
+        foreach(var vert in vertices)
+        {
+            color = new Color(1, vert.y / 4, 1, 1.0f);
+            GameObject sphere = Instantiate(verticePrefab, vert, Quaternion.identity, verticeParent);
+            sphere.GetComponent<MeshRenderer>().material.color = color;
+        }
+    }
+    public void showHideVertices()
+    {
+        verticeParent.gameObject.SetActive(!verticeParent.gameObject.activeSelf);
+        generateButton.interactable = verticeParent.gameObject.activeSelf;
+    }
+}
---
+++ Assets/Scripts/LogCarver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogCarver : MonoBehaviour
+{
+    private Vector3[] vertices;
+    [SerializeField]
+    private InputField startInput, endInput, multInput;
+    public void carveLog(float heightStart, float heightEnd, float multiplier)
+    {
+        vertices = GetComponent<MeshFilter>().mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            if(vertices[i].y >= heightStart && vertices[i].y < heightEnd)
+            {
+                vertices[i].x *= multiplier;
+                vertices[i].z *= multiplier;
+            }
+        }
+        GetComponent<MeshFilter>().mesh.vertices = vertices;
+    }
+    public void carveLog()
+    {
+        vertices = GetComponent<MeshFilter>().mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            if (vertices[i].y >= float.Parse(startInput.text) && vertices[i].y < float.Parse(endInput.text))
+            {
+                vertices[i].x *= float.Parse(multInput.text);
+                vertices[i].z *= float.Parse(multInput.text);
+            }
+        }
+        GetComponent<MeshFilter>().mesh.vertices = vertices;
+    }
+}
